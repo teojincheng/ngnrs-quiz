4e96cf1d5af28785723bb3a0fb3f305d730f9e81
teojincheng <jinchengproject@gmail.com>
2020-05-01 10:18:42 +0800

    [jc] update form with onblur event

--- src/fancy_form/script.js
+++ src/fancy_form/script.js
@@ -1,11 +1,23 @@
-function validateForm() {
+function validateAddress() {
   let inputAddressValue = document.getElementById("input-address").value;
 
   let isInputAddressValid = WAValidator.validate(inputAddressValue, "bitcoin");
 
   if (!isInputAddressValid) {
-    alert("Bitcoin address is invalid");
+    document.getElementById("address-validation-msg").innerText =
+      "Invalid address";
+    document.getElementById("address-validation-msg").style.visibility =
+      "visible";
     return false;
+  } else {
+    document.getElementById("address-validation-msg").style.visibility =
+      "hidden";
+    document.getElementById("address-validation-msg").innerText = "message";
+    return true;
   }
-  return true;
+}
+
+function validateForm() {
+  let isAddresValid = validateAddress();
+  return isAddresValid;
 }
