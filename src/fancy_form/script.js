17040a349a16aeb6e5a7f1d74e3534a08792df3b
teojincheng <jinchengproject@gmail.com>
2020-05-01 10:50:19 +0800

    [jc] added in code checkOTP field is not empty

--- src/fancy_form/script.js
+++ src/fancy_form/script.js
@@ -17,7 +17,34 @@ function validateAddress() {
   }
 }
 
+function validateEmptyElement(elementId) {
+  let elementValue = document.getElementById(elementId).value;
+  if (elementValue === "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateAmount() {
+  let isAmountEmpty = validateEmptyElement("input-amount");
+}
+
+function validateOTP() {
+  let isOTPEmpty = validateEmptyElement("input-otp");
+  if (isOTPEmpty) {
+    document.getElementById("otp-validation-msg").innerText = "Cannot be empty";
+    document.getElementById("otp-validation-msg").style.visibility = "visible";
+    return false;
+  } else {
+    document.getElementById("otp-validation-msg").style.visibility = "hidden";
+    document.getElementById("otp-validation-msg").innerText = "message";
+    return true;
+  }
+}
+
 function validateForm() {
   let isAddresValid = validateAddress();
+  let isOTPNotEmpty = validateOTP();
   return isAddresValid;
 }
