b055083a624c01b6d11696f1f17d2caae28951d7
teojincheng <jinchengproject@gmail.com>
2020-05-02 09:14:07 +0800

    [jc ] update datasource.class to also use substr()

--- src/datasource_connector_tool/Datasource.js
+++ src/datasource_connector_tool/Datasource.js
@@ -1,5 +1,17 @@
 const ENDPOINT_URL = "https://static.ngnrs.io/test/prices";
+
+/**
+ * Javascript class
+ * So, in a node.js project, this can class exists in one js file
+ * and we can use module.exports to export this class.
+ */
 class Datasource {
+  /**
+   * getPrices is a function that returns a promise which
+   * resolves the response of a xmlhttprequest.
+   *
+   */
+
   getPrices() {
     var xhttp = new XMLHttpRequest();
     return new Promise(function (resolve, reject) {
@@ -10,9 +22,16 @@ class Datasource {
           let resultArr = [];
           for (let i = 0; i < arrOfCurrencyPair.length; i++) {
             let currPairObj = {};
+            const lastIndex = arrOfCurrencyPair[i].pair.length;
+            const startIndex = arrOfCurrencyPair[i].pair.length - 3;
+            let newQuote = arrOfCurrencyPair[i].pair.substring(
+              startIndex,
+              lastIndex
+            );
             currPairObj.pair = arrOfCurrencyPair[i].pair;
             currPairObj.mid =
-              (arrOfCurrencyPair[i].buy + arrOfCurrencyPair[i].sell) / 2;
+              (arrOfCurrencyPair[i].buy + arrOfCurrencyPair[i].sell) / 2 / 100;
+            currPairObj.quote = newQuote;
             resultArr.push(currPairObj);
           }
           resolve(resultArr);
