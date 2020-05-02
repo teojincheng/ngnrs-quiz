d8847b33c01e04d98a0c9be88c13ea84c2fccc84
teojincheng <jinchengproject@gmail.com>
2020-05-02 10:07:42 +0800

    [jc] apply binding to class inside getPrices() function

--- src/datasource_connector_tool/Datasource.js
+++ src/datasource_connector_tool/Datasource.js
@@ -6,13 +6,20 @@ const ENDPOINT_URL = "https://static.ngnrs.io/test/prices";
  * and we can use module.exports to export this class.
  */
 class Datasource {
+  generateQuote(currencyPairStr) {
+    const lastIndex = currencyPairStr.length;
+    const startIndex = currencyPairStr.length - 3;
+    let quote = currencyPairStr.substring(startIndex, lastIndex);
+    return quote;
+  }
+
   /**
    * getPrices is a function that returns a promise which
    * resolves the response of a xmlhttprequest.
    *
    */
-
   getPrices() {
+    let self = this;
     var xhttp = new XMLHttpRequest();
     return new Promise(function (resolve, reject) {
       xhttp.onreadystatechange = function () {
@@ -22,12 +29,7 @@ class Datasource {
           let resultArr = [];
           for (let i = 0; i < arrOfCurrencyPair.length; i++) {
             let currPairObj = {};
-            const lastIndex = arrOfCurrencyPair[i].pair.length;
-            const startIndex = arrOfCurrencyPair[i].pair.length - 3;
-            let newQuote = arrOfCurrencyPair[i].pair.substring(
-              startIndex,
-              lastIndex
-            );
+            let newQuote = self.generateQuote(arrOfCurrencyPair[i].pair);
             currPairObj.pair = arrOfCurrencyPair[i].pair;
             currPairObj.mid =
               (arrOfCurrencyPair[i].buy + arrOfCurrencyPair[i].sell) / 2 / 100;
